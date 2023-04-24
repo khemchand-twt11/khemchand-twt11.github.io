1239c25777366bd19a60b960cedc53a38d7e94b6
khemchand-twt-11 <gauravsharma6290852476@gmail.com>
2023-04-25 00:55:36 +0530

    changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-gsap.to("#basic-info", { duration: 1.7, ease: "back.out(1.5)", x: 120 });
+gsap.to("#basic-info >div:nth-child(1)", {
+  duration: 1.7,
+  ease: "back.out(1.5)",
+  x: 80,
+});
+
+const menu = document.getElementById("menu");
+const navbar = document.getElementById("nav-menu");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  menu.classList.toggle("active");
+});
