9273aac97623515c7851106c6b9d3f72ebcd04fa
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-05-12 14:56:42 +0530

    changes

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ gsap.to("#basic-info >div:nth-child(1)", {
   ease: "back.out(1.5)",
   x: 80,
 });
-
-const menu = document.getElementById("menu");
-const navbar = document.getElementById("nav-menu");
-
-menu.addEventListener("click", () => {
-  navbar.classList.toggle("show");
-  menu.classList.toggle("active");
-});
