d7d006c67fe999ec135eb68cb515748c28e3ec3c
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-06-21 02:10:13 +0530

    email functionallity

--- index.js
+++ index.js
@@ -19,3 +19,29 @@ function handleResize() {
 }
 
 window.addEventListener("resize", handleResize);
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const message = document.getElementById("message").value;
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = sID;
+  const templateID = tID;
+
+  emailjs.send(serviceID, templateID, params).then((res) => {
+    name.value = "";
+    email.value = "";
+    message.value = "";
+    console.log(res);
+    document.getElementById("name").value,
+      document.getElementById("email").value,
+      document.getElementById("message").value,
+      alert("your message sent successfy");
+  });
+}
